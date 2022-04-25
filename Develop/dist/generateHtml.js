ff5aaf87e1eaeff6dd2b827bb20eeac2af62ae21
walterunderwood <walonsounderwood@gmail.com>
2022-04-25 15:48:49 -0700

    Creating generateManager function

--- Develop/dist/generateHtml.js
+++ Develop/dist/generateHtml.js
@@ -1,3 +1,25 @@
 const generateTeam = (team) => {
-    console.log (team);
+    
+    const indexHtml = [];
+
+    const generateManager = manager => {
+        let managerHtml = `
+        
+        <div class="card shadow-lg p-3 mb-5 rounded" style="width: 18rem;">
+                <div class="card-header">
+                    ${manager.name} <br />
+                    &#128084; Manager
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${manager.id}</li>
+                    <li class="list-group-item">Email: <span id="email"><a
+                                href="mailto:${manager.email}">${manager.email}</a></span></li>
+                    <li class="list-group-item"> Office Number: ${manager.officeNumber}</li>
+                </ul>
+            </div>
+        
+        `;
+        indexHtml.push(managerHtml);
+    }
+    
 }    
